db7a06686f2c355e6b50ee5d88a350b02bc7e7b1
Lesedi Chirikuutsi <lc13@williams.edu>
2025-03-28 20:31:55 -0400

    Add homepage, image, and HomeController

---
+++ src/main/java/com/teaknpine/teaknpine/TeaknpineApplication.java
@@ -0,0 +1,13 @@
+package com.teaknpine.teaknpine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaknpineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaknpineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teaknpine/teaknpine/TeaknpineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teaknpine.teaknpine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaknpineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
